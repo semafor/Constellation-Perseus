cf8eabffe7c689f0c8ef8cd7ec21a493f98643fa
Jonas Grønås Drange <jonas@drange.net>
2014-02-17 20:48:17 +0100

    plz

--- src/no/jgdx/perseus/ships/harvesters/HarvesterClassification.java
+++ src/no/jgdx/perseus/ships/harvesters/HarvesterClassification.java
@@ -6,5 +6,6 @@ public enum HarvesterClassification {
 	CARBON_COLLECTOR, PHOSPORUS_MINER, OXYGEN_MINER, SULFUR_MINER, SELENIUM_MINER,
 
 	THAT_SHIELD_AROUND_THE_STAR_THING_JONAS_MENTIONED;
+	/* the Dyson sphere? We don't need that yet. */
 
 }
